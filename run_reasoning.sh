af1daa4efec6dca1840e6f0274d4e5f39ee2aac5
sklim84 <captiong84@gmail.com>
2026-01-14 19:01:27 +0900

    add claude evaluation results

--- run_reasoning.sh
+++ run_reasoning.sh
@@ -1,8 +1,10 @@
 # 타겟 모델별 reasoning answer 생성
 # opensource llm
-python3 -u eval/2_1_gen_reasoning_openlm.py
+# python3 -u eval/2_1_gen_reasoning_openlm.py
 # proprietary (OpenAI) llm
 # python3 -u eval/2_1_gen_reasoning_openai.py
+# proprietary (Anthropic) llm
+# python3 -u eval/2_1_gen_reasoning_claude.py
 
 # 타겟 모델별 reasoning answer 평가
-# python3 -u eval/2_2_eval_reasoning_openai.py
+python3 -u eval/2_2_eval_reasoning_openai.py
